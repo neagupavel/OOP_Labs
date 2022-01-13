f1a0f15545e899f1896616d3f6aa941c6b084948
neagupavel <91536591+neagupavel@users.noreply.github.com>
2022-01-13 15:27:29 +0200

    lab3 task1

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
-package Lab2.task1;
+package Lab3.task1;
 
 public class Main {
     public static void main(String[] args) {
-        Box box1 = new Box();
-        Box box2 = new Box(5);
-        Box box3 = new Box(5, 4, 3);
+        String text = "Object-oriented programming (OOP) is a programming paradigm based on the concept of " +
+                "\"objects\", which can contain data and code: data in the form of fields (often known as attributes " +
+                "or properties), and code, in the form of procedures (often known as methods).\n" +
+                "\n" +
+                "A feature of objects is that an object's own procedures can access and often modify the data fields " +
+                "of itself (objects have a notion of this or self). In OOP, computer programs are designed by making " +
+                "them out of objects that interact with one another.[1][2] OOP languages are diverse, but the most " +
+                "popular ones are class-based, meaning that objects are instances of classes, which also determine " +
+                "their types.\n" +
+                "\n" +
+                "Many of the most widely used programming languages (such as C++, Java, Python, etc.) are " +
+                "multi-paradigm and they support object-oriented programming to a greater or lesser degree, typically" +
+                " in combination with imperative, procedural programming. Significant object-oriented languages " +
+                "include: Java, C++, C#, Python, R, PHP, Visual Basic.NET, JavaScript, Ruby, Perl, SIMSCRIPT, Object " +
+                "Pascal, Objective-C, Dart, Swift, Scala, Kotlin, Common Lisp, MATLAB, and Smalltalk.";
 
-        System.out.println(String.valueOf(box1.getSurface()) + ' ' + String.valueOf(box1.getVolume()));
-        System.out.println(String.valueOf(box2.getSurface()) + ' ' + String.valueOf(box2.getVolume()));
-        System.out.println(String.valueOf(box3.getSurface()) + ' ' + String.valueOf(box3.getVolume()));
+        System.out.println(TextManipulator.getNrOfSentences(text));
+        System.out.println(TextManipulator.getNrOfWords(text));
+        System.out.println(TextManipulator.getNrOfVowels(text));
+        System.out.println(TextManipulator.getNrOfConsonants(text));
+        System.out.println(TextManipulator.getMostOftenWords(text, 5));
+        System.out.println(TextManipulator.getLongestWord(text));
     }
 }
